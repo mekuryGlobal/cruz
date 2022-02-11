444cc81f889093b87d7d8f8b9d2c3d4703bed68f
Faye <96606732+Fayhee@users.noreply.github.com>
2022-02-11 03:10:25 +0100

    This commit includes the rendering of the player

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 mod map;
-mod prelude{
-    pub use bracket_lib:: prelude::*;
+mod player;
+mod prelude {
+    pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
-use prelude::*; 
+use prelude::*;
 
-struct State{
-    map:Map
+struct State {
+    map: Map,
+    player: Player,
 }
 
-impl State{
-    fn new() -> Self{
-        Self{map: Map::new()}
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new(),
+        player: Player::new(
+            point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        ),
+    }
+        
     }
 }
 
-impl GameState for State{
-    fn tick (&mut self, ctx: &mut BTerm){
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         self.map.render(ctx);
     }
 }
 
-
-
-
-fn main() -> BError{
-
-   let context = BTermBuilder::simple80x50()
-   .with_title("Cruz")
-   .with_fps_cap(30.0)
-   .build()?;
-   main_loop(context, State::new())
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Cruz")
+        .with_fps_cap(30.0)
+        .build()?;
+    main_loop(context, State::new())
 }
-
